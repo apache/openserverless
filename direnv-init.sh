0cf26322fd47fa9b8cb14c7267cb84e825ae81fd
Michele Sciabarra <30654959+sciabarracom@users.noreply.github.com>
2024-07-22 16:33:51 +0100

    updated workspaces and direnv (#24)
    * updated workspaces and direnv
    
    * update readme
    
    ---------
    
    Co-authored-by: Michele Sciabarra <michele@sciabarra.com>

--- direnv-init.sh
+++ direnv-init.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")" && pwd
+git clone https://github.com/nix-community/nix-direnv $HOME/nix-direnv
+mkdir -p $HOME/.config/direnv
+echo 'source $HOME/nix-direnv/direnvrc' >>$HOME/.config/direnv/direnvrc
 find . -name '.envrc' -execdir direnv allow . pwd  \;
 find . -name '.envrc' -execdir direnv exec . pwd  \;
 
