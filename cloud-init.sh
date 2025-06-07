5ca5df943ba29d153ca2e64ad7a7863f27712d81
Michele Sciabarra <msciabarra@apache.org>
2025-06-07 16:32:43 +0200

    guards

--- cloud-init.sh
+++ cloud-init.sh
@@ -2,21 +2,22 @@
 IP="$(ip -4 -o addr | awk  '$2 ~ /en/ {print $4}' | cut -d/ -f1)"
 
 #  setup k3s
-curl -sfL https://get.k3s.io | sudo sh -
+if ! which k3s
+then curl -sfL https://get.k3s.io | sudo sh -
+fi
 
 # kubeconfig with local ip
-mkdir -p "$HOME/.kube"
-sudo cat  /etc/rancher/k3s/k3s.yaml |\
- sed -e 's!server: https://127.0.0.1:6443!server: https://'$IP':6443!' \
- >"$HOME/.kube/config"
 
-echo "export KUBECONFIG=$HOME/.kube/config" >> "$HOME/.bashrc"
+if ! test -f "$HOME/.kube/config"
+then
+    mkdir -p "$HOME/.kube"
+    sudo cat  /etc/rancher/k3s/k3s.yaml |\
+    sed -e 's!server: https://127.0.0.1:6443!server: https://'$IP':6443!' \
+    >"$HOME/.kube/config"
+    echo "export KUBECONFIG=$HOME/.kube/config" >> "$HOME/.bashrc"
+fi
+
 kubectl get nodes
 
-## not needed yet
-# setup ops
-#curl -sL bit.ly/get-ops | bash
-#source ~/.bashrc
-#ops -t
 
 echo "=== DONE ===" 
